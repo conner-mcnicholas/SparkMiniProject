0c1a25a44b451965c0c61052d407c6755992bdd2
conner-mcnicholas <connermcnicholas@gmail.com>
2022-06-09 10:11:07 -0700

    logs

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 case "$PYSPARK_DRIVER_PYTHON" in  "jupyter") unset PYSPARK_DRIVER_PYTHON; esac
-spark-submit autoinc_pyspark.py > execution_log.txt
+spark-submit autoinc_pyspark.py
 echo '\nsingle file results printed to annual_acc.txt'
 echo '\nresults saved as text file in HDFS format in HDFS_text_results folder'
